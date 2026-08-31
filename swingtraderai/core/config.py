7636a55b86ce3547cca84afeead9441c20710cf8
Bulat Nasyrov <bulatnasirov2003@gmail.com>
2026-08-31 15:24:15 +0300

    Removed unnecessary variables

--- swingtraderai/core/config.py
+++ swingtraderai/core/config.py
@@ -13,8 +13,6 @@ class Settings(BaseSettings):
 	ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 	REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 	CORS_ALLOWED_ORIGINS: list[str] = []
-	KAGGLE_USERNAME: str
-	KAGGLE_KEY: str
 
 
 settings = Settings()
